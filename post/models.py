74b8231c04c660378f8e044ab14961ba04212f26
AyahHesham <ayahhesham0@gmail.com>
2022-08-24 09:54:41 +0200

    add auther to model

--- post/models.py
+++ post/models.py
@@ -1,10 +1,12 @@
 from distutils.command import upload
 from statistics import mode
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class post(models.Model):
+    auther=models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_post')
     title=models.CharField(max_length=100)
     continant=models.CharField(max_length=200)
     image=models.ImageField(upload_to='post')
